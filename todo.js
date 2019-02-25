4ec1ab1efa86387c07b1450572a2d7133c688136
Sean Kelley <seangtkelley@gmail.com>
2019-02-25 16:25:42 -0500

    fixed

--- todo.js
+++ todo.js
@@ -2,6 +2,6 @@ const addItem = () => {
     let textBox = document.getElementById('add-todo-text');
     let taskList = document.getElementById('todo-list');
     let newTask = document.createElement("LI");
-    newTask.innerText = textBox.nodeValue;
+    newTask.innerText = textBox.value;
     taskList.appendChild(newTask);
 };
